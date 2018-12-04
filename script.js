f2a6cc76c89020a10d101daca4fee6a103ebe61f
László Horváth <bedafoltyn@gmail.com>
2018-12-04 17:59:12 +0100

    share functions removed, content changes

--- script.js
+++ script.js
@@ -44,33 +44,6 @@ function navigateToPage (event) {
 
 navigationButtons.forEach(button => button.addEventListener('click', navigateToPage))
 
-
-// SHARE
-const facebookShareButton = document.querySelector('.social-button.facebook')
-const tumblrShareButton = document.querySelector('.social-button.tumblr')
-const twitterShareButton = document.querySelector('.social-button.twitter')
-const { URL } = document
-const windowSize = 'height=350,width=600'
-
-function shareOnFacebook () {
-  const popup = window.open(`https://www.facebook.com/sharer/sharer.php?u=${URL}`, 'facebook-popup', windowSize)
-  if (popup.focus) popup.focus()
-}
-
-function shareOnTumblr () {
-  const popup = window.open(`https://tumblr.com/widgets/share/tool?canonicalUrl=${URL}&posttype=link`, 'tumblr-popup', windowSize)
-  if (popup.focus) popup.focus()
-}
-
-function shareOnTwitter () {
-  const popup = window.open(`https://twitter.com/share?url=${URL}`, 'twitter-popup', windowSize)
-  if (popup.focus) popup.focus()
-}
-
-facebookShareButton.addEventListener('click', shareOnFacebook)
-tumblrShareButton.addEventListener('click', shareOnTumblr)
-twitterShareButton.addEventListener('click', shareOnTwitter)
-
 // HAMBURGER
 $('.hamburger').on('click', function () {
   $(this).toggleClass('is-active')
